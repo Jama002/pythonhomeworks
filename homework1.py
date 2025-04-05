4e6204af4c5984f4ad1a790c9ea6d1adf98a7e01
unknown <mavlonovj0222@gmail.com>
2025-04-05 10:24:56 +0500

    first commit

---
+++ homework1.py
@@ -0,0 +1,23 @@
+#Homework 1
+import math
+#Task1
+n = int(input('enter length'))
+print(f'Area of square: {n**2}')
+print(f'Perimeter of square: {n*4}')
+
+#Task2
+diameter = int(input('enter diameter'))
+print(f'Length of circle is: {diameter * math.pi}')
+
+#task3
+a = int(input('enter first number'))
+b = int(input('enter first second'))
+print(f'{a} and {b} mean is {(a+b)/2}')
+
+#Task4
+c = int(input('enter first number'))
+d = int(input('enter first second'))
+print(f'sum of {c} and {d} is {c+d}')
+print(f'product of {c} and {d} is {c*d}')
+print(f'square of {c} is {c**2}')
+print(f'square of {d} is {d**2}')
